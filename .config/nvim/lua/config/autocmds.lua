76f164a01dd0b674e0f487bbea5bfe76d1529146
Apollon002 <accounts@jannikzenker.de>
2025-12-12 15:21:14 +0100

    Switched to LazyVim

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,37 +1,8 @@
--- Beim Eintritt in Insert-Mode: absolute Nummern
-local acmd = vim.api.nvim_create_autocmd
-
-acmd("InsertEnter", {
-	callback = function()
-		vim.wo.number = true
-		vim.wo.relativenumber = false
-	end,
-})
-
--- Beim Verlassen von Insert-Mode: wieder relative Nummern
-acmd("InsertLeave", {
-	callback = function()
-		vim.wo.number = true
-		vim.wo.relativenumber = true
-	end,
-})
-
---- set up linters
-acmd({ "BufWritePost", "BufEnter", "InsertLeave" }, {
-	callback = function()
-		require("lint").try_lint()
-	end,
-})
-
---- open csv with csvview and without soft wrapping ---
-acmd("FileType", {
-	pattern = { "csv" }, -- ggf. erweitern: { "csv", "tsv" }
-	callback = function()
-		-- kein Softwrap
-		vim.opt_local.wrap = false
-		vim.opt_local.linebreak = false
-
-		-- csvview mit gewünschten Optionen einschalten
-		vim.cmd([[CsvViewToggle delimiter=, display_mode=border header_lnum=1]])
-	end,
-})
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,13 +1,3 @@
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
-
---- configure diagnostics ---
-vim.diagnostic.config({
-	virtual_text = true, -- Nachrichten rechts am Zeilenende
-	signs = true, -- Icons / Buchstaben links in der SignColumn
-	underline = true, -- Unterstreichungen im Code
-	update_in_insert = false, -- nicht mitten im Tippen stören
-	severity_sort = true, -- sortiert nach Fehlerstufe
-})
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
